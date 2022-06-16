5a97c9fb4b41343b76bc58666d07cf21167b11bd
Nischal Shakya <nischalshakya2015@gmail.com>
2022-06-16 23:14:36 +0545

    Add custom propertyImage type

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import objects and document schema
 import property from './property'
+import propertyImage from './propertyImage'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -15,6 +16,7 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    property
+    property,
+    propertyImage
   ])
 })
